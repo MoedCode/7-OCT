6044a073b531c3a1cdac5bf0d915b91129f3d03f
MoedCode <mohamed71291@gmail.com>
2024-06-23 15:40:10 +0300

    Sun Jun 23 15:40:10 EEST 2024

---
+++ 7-oct-env/dependances.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+# Install Django
+pip3 install django
+
+# Install MySQL client for Django
+pip3 install mysqlclient
+
+# Install SQLAlchemy
+pip3 install sqlalchemy
+
+# Install Django REST framework
+pip3 install djangorestframework
