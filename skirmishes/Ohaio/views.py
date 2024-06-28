07c0b0a4cafaaf1d6d424c48eaa78c0bcbc63e9c
MoedCode <mohamed71291@gmail.com>
2024-06-28 16:32:17 +0300

    Fri Jun 28 16:32:17 EEST 2024

--- skirmishes/Ohaio/views.py
+++ skirmishes/Ohaio/views.py
@@ -2,13 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-str = ''' <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus
-alias odio, id, voluptas accusantium eos vero labore cumque nihil quod ipsa et omnis
-voluptatem accusamus tenetur consequatur deserunt quas in.</h1>'''
+
 def index(request):
     return render(request, "Ohaio/index.html")
 def index1(request):
-    return HttpResponse(str)
+    return render(request, "Ohaio/index1.html")
 def great(request, name):
     return HttpResponse( f"<h1> Hello , {name} </h1> {request.__dict__}")
 def useless(request, text, number):
