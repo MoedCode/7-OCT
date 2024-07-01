1c5219d89e4476dcd80dc46d4a7092bf7b28767f
MoedCode <mohamed71291@gmail.com>
2024-07-01 12:56:13 +0300

    Mon Jul  1 12:56:13 EEST 2024

--- Xtwittes_backend/authentication/models.py
+++ Xtwittes_backend/authentication/models.py
@@ -6,10 +6,13 @@ User = get_user_model()
 # Create your models here.
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    user_id = models.IntegerField()
+    # user_id = models.IntegerField()
     bio = models.TextField(blank=True)
-    Profile = models.ImageField(upload_to='profile_images', default='book-icon.png')
+    Profile_img = models.ImageField(upload_to='profile_images%y%m%d', default='book-icon.png')
     location = models.CharField(max_length=100, blank=True)
+
+    def __str__(self):
+        return self.user.username
 if __name__ == "__main__":
     user0 = User()
     user1 = User(name="kapaka", age=25)
