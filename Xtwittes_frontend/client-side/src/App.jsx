42e306e4b45fed5b07c0307e8c84f1f601d77454
salah-koulal <salah.rajawi289@gmail.com>
2024-07-07 14:35:23 +0100

    🥷🏼 MakingFriendsCard

--- Xtwittes_frontend/client-side/src/App.jsx
+++ Xtwittes_frontend/client-side/src/App.jsx
@@ -1,15 +1,16 @@
 import {Outlet, Navigate, Route, Routes, useLocation } from "react-router-dom";
 import {useSelector} from "react-redux"
 import { Home, Login, Profile, Register, ResetPassword } from "./pages";
-
+import { user } from "./assets/data.js";
 
 function Layout(){
   const {user} = useSelector(state => state.user) ;
   const location = useLocation();
+  // console.log({user}); 
   return user?.token?(
     <Outlet/>
   ): (
-    <Navigate to='/login' state= {{from: location}} replace/>
+    <Navigate to='/home' state= {{from: location}} replace/>
   );
 }
 function App() {
